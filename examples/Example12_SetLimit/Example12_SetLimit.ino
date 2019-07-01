d08f7d4ca140d4081dc66b2ec92bc18896a1b14d
Nathan Seidle <nathan.seidle@sparkfun.local>
2019-07-01 11:13:58 -0600

    Update Example12_SetLimit.ino

--- examples/Example12_SetLimit/Example12_SetLimit.ino
+++ examples/Example12_SetLimit/Example12_SetLimit.ino
@@ -8,7 +8,7 @@
 
   This example shows how to set the max encoder value. For example,
   setting the limit to 359 will not allow the encoder read values to
-  go above 359, looping back to zero instead. Useful when the encoder 
+  go above 359, looping back to zero instead. Useful when the encoder
   value is mapped directly onto a volume setting, FM freq, etc.
 
   This feature is available on Qwiic Twist firmware v1.2 and above.
@@ -35,12 +35,19 @@ void setup() {
     while (1);
   }
 
-  if(twist.getVersion() < 0x0201) //v1.2 in two byte form
+  int currentVersion = twist.getVersion();
+
+  if (currentVersion < 0x0201) //v1.2 in two byte form
   {
+    Serial.print("The current firmware version is: ");
+    Serial.print(currentVersion & 0xFF);
+    Serial.print(".");
+    Serial.println(currentVersion >> 8);
+
     Serial.println("This feature is not supported. Please consider upgrading the firmware on your Qwiic Twist. Freezing.");
-    while(1); //Freeze
+    while (1); //Freeze
   }
-  
+
   twist.setLimit(40); //Set max encoder to 40. It will loop to zero after 11 ticks.
   //twist.setLimit(0); //Disable this feature
 
